ff1361e4e4c60c6008a350845e8780a69d7b92c0
nicolas098 <nicolas_cuadros82162@elpoli.edu.co>
2025-05-25 18:21:39 -0500

    version 2.1 puerto corregido

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def report_summary():
 import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',10000))
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=port)
 
 
 
